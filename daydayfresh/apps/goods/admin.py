a190c41c369fc85304264ef834f6230d446f901a
YouZei <youzei@email.com>
2019-10-23 01:32:19 +0800

    V 1.0.3

--- daydayfresh/apps/goods/admin.py
+++ daydayfresh/apps/goods/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from goods.models import GoodsType,IndexGoodsBanner,GoodsSKU,Goods,GoodsImage,IndexPromotionBanner,IndexTypeGoodsBanner
 
 # Register your models here.
 
 
 admin.site.register(GoodsType)
+admin.site.register(IndexGoodsBanner)
+admin.site.register(GoodsSKU)
+admin.site.register(Goods)
+admin.site.register(GoodsImage)
+admin.site.register(IndexPromotionBanner)
+admin.site.register(IndexTypeGoodsBanner)
--- daydayfresh/celery_tasks/tasks.py
+++ daydayfresh/celery_tasks/tasks.py
@@ -3,7 +3,6 @@
 from django.core.mail import send_mail
 from django.conf import settings
 from celery import Celery
-import time
 # 在任务处理者(这里放在数据库所在服务器里面)一端加这几句
 # import os
 # import django
@@ -11,7 +10,7 @@ import time
 # django.setup()
 
 # 创建一个Celery实例对象
-app = Celery('celery_tasks.tasks', broker='redis://192.168.13.200:6380/1')
+app = Celery('celery_tasks.tasks', broker='redis://192.168.1.200:6379/0')
 
 
 # 定义任务函数
@@ -27,4 +26,3 @@ def send_register_active_email(to_email, username, token):
     html_message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击下面链接激活您的账户<br/><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (
         username, token, token)
     send_mail(subject, massage, sender, receiver, html_message=html_message)
-    time.sleep(5)
